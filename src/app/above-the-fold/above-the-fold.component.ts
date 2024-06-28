7eff9dfc4b647bf7d7b4a374682d74d027267696
peet <teilzeitjobbewerber@gmx.de>
2024-06-28 12:14:55 +0200

    more stuff done

--- src/app/above-the-fold/above-the-fold.component.ts
+++ src/app/above-the-fold/above-the-fold.component.ts
@@ -9,4 +9,15 @@ import { Component } from '@angular/core';
 })
 export class AboveTheFoldComponent {
 
+  constructor(){
+    for (let i = 1; i < 4; i++) {
+      setTimeout(() => {
+        this.arrowN = i
+        console.log(this.arrowN);
+      }, 4000);
+    }
+  }
+  
+  arrowN : number= 1
+
 }
