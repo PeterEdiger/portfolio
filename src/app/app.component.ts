575a27b7bb7b5c2e54e61bbf47a08c0cbb03c046
peet <teilzeitjobbewerber@gmx.de>
2024-07-07 13:48:05 +0200

    functioning version img added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { AboveTheFoldComponent } from './above-the-fold/above-the-fold.component
 import { AboutMeComponent } from './about-me/about-me.component';
 import { MySkillsComponent } from './my-skills/my-skills.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
 
 @Component({
@@ -19,6 +20,7 @@ import { PortfolioComponent } from './portfolio/portfolio.component';
     AboutMeComponent,
     MySkillsComponent,
     PortfolioComponent,
+    ContactFormComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
