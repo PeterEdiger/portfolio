d3ad2857c076f8697b17c2ca973a2b2df7da8ef1
peet <teilzeitjobbewerber@gmx.de>
2024-07-08 10:41:34 +0200

    submit button in progress

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.scss'
 })
 export class ContactFormComponent {
 
+  contactData = {
+    name: "",
+    email: "",
+    message: ""
+  };
+
+  onSubmit() {
+    console.log("runs");
+  }
 }
