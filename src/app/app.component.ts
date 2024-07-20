f24b4634e263c07fd7c24f4b5eaee1be1a8d0498
peet <teilzeitjobbewerber@gmx.de>
2024-07-20 13:55:55 +0200

    responsivness and linking

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router, NavigationEnd } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
-import { AboveTheFoldComponent } from './above-the-fold/above-the-fold.component';
-import { AboutMeComponent } from './about-me/about-me.component';
-import { MySkillsComponent } from './my-skills/my-skills.component';
-import { PortfolioComponent } from './portfolio/portfolio.component';
-import { ContactFormComponent } from './contact-form/contact-form.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { MainPageComponent } from './main-page/main-page.component';
 
@@ -24,6 +19,18 @@ import { MainPageComponent } from './main-page/main-page.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'portfolio';
+  isImprintRoute: boolean = false;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.isImprintRoute = event.urlAfterRedirects === '/imprint';
+      }
+    });
+  }
 }
