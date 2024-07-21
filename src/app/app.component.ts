539b089b96d45fb2e65efcc1507a0c13fd89b659
peet <teilzeitjobbewerber@gmx.de>
2024-07-21 13:47:53 +0200

    dd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,6 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
-import { AboveTheFoldComponent } from './above-the-fold/above-the-fold.component';
-import { AboutMeComponent } from './about-me/about-me.component';
-import { MySkillsComponent } from './my-skills/my-skills.component';
-import { PortfolioComponent } from './portfolio/portfolio.component';
-import { ContactFormComponent } from './contact-form/contact-form.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { MainPageComponent } from './main-page/main-page.component';
 
