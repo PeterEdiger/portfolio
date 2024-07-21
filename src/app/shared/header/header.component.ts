cecb704ad354ddd8533a9de419d08fba884a0ebc
peet <teilzeitjobbewerber@gmx.de>
2024-07-21 16:26:36 +0200

    moving on with translate

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,26 +1,44 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterLink,
+    RouterOutlet,
+    TranslateModule
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
 
-  visibility = true
-
-  burgerToggle(){
-    if(this.visibility){
-      document.body.style.overflow = 'hidden';
-      this.visibility = false
-    }
-  else{
-    document.body.style.overflow = '';
-    this.visibility = true
+  constructor(private translate: TranslateService){
+    this.translate.setDefaultLang("en");
+    this.translate.addLangs(["en", "de"])
+    this.translate.use("en")
   }
-}
+
+
+  switchLanguage(language: string){
+    this.translate.use(language)
+  }
+
+
+  visibility = true;
+
+  burgerToggle() {
+    if (this.visibility) {
+      document.body.style.overflow = 'hidden';
+      this.visibility = false;
+    }
+    else {
+      document.body.style.overflow = '';
+      this.visibility = true;
+    }
+  }
 }
